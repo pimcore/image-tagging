a0dfd1f0aec52dd883850352a12f873de475ca35
Christian Fasching <christian.fasching@elements.at>
2019-09-17 10:57:08 +0200

    fixed GUI when no model

--- src/Resources/public/js/startup.js
+++ src/Resources/public/js/startup.js
@@ -34,56 +34,58 @@ pimcore.bundle.imageTaggingBundle = Class.create(pimcore.plugin.admin, {
                 success: function (asset, response) {
                     var data = Ext.decode(response.responseText);
 
-                    if (data.success && data.models.length > 0) {
+                    if (data.success) {
 
-                        var items = [];
+                        if(data.models.length > 0) {
+                            var items = [];
 
-                        for(var i = 0; i < data.models.length; i++) {
-                            items.push({
-                                text: data.models[i].nicename,
-                                iconCls: 'image_tagging_model_white',
-                                handler: function (asset, model) {
-                                    asset.tab.mask();
-                                    Ext.Ajax.request({
+                            for(var i = 0; i < data.models.length; i++) {
+                                items.push({
+                                    text: data.models[i].nicename,
+                                    iconCls: 'image_tagging_model_white',
+                                    handler: function (asset, model) {
+                                        asset.tab.mask();
+                                        Ext.Ajax.request({
 
-                                        url: '/admin/bundle/image-tagging/classify',
-                                        method: 'post',
-                                        params: {
-                                            'id': asset.id,
-                                            'model': model.name,
-                                            'version': model.version
-                                        },
-                                        success: function (asset, response) {
-                                            asset.tab.unmask();
-                                            if (data.success) {
-                                                asset.tabbar.setActiveItem(asset.tagAssignment.getLayout());
-                                                asset.tagAssignment.grid.store.reload();
-                                                asset.tagAssignment.layout.items.get(1).items.get(0).store.reload();
+                                            url: '/admin/bundle/image-tagging/classify',
+                                            method: 'post',
+                                            params: {
+                                                'id': asset.id,
+                                                'model': model.name,
+                                                'version': model.version
+                                            },
+                                            success: function (asset, response) {
+                                                asset.tab.unmask();
+                                                if (data.success) {
+                                                    asset.tabbar.setActiveItem(asset.tagAssignment.getLayout());
+                                                    asset.tagAssignment.grid.store.reload();
+                                                    asset.tagAssignment.layout.items.get(1).items.get(0).store.reload();
 
-                                                pimcore.helpers.showNotification(t("image_tagging_auto_classification"), t("image_tagging_auto_classification_tagging_successful"), "success");
-                                            } else {
-                                                pimcore.helpers.showNotification(t("error"), t("image_tagging_auto_classification_tagging_failed"), "error");
-                                            }
-                                        }.bind(this, asset)
+                                                    pimcore.helpers.showNotification(t("image_tagging_auto_classification"), t("image_tagging_auto_classification_tagging_successful"), "success");
+                                                } else {
+                                                    pimcore.helpers.showNotification(t("error"), t("image_tagging_auto_classification_tagging_failed"), "error");
+                                                }
+                                            }.bind(this, asset)
 
-                                    });
+                                        });
 
-                                }.bind(this, asset, data.models[i])
+                                    }.bind(this, asset, data.models[i])
+                                });
+                            }
+
+                            asset.toolbar.add({
+                                text: t('image_tagging_auto_classification'),
+                                scale: "medium",
+                                iconCls: 'image_tagging_white',
+                                cls: 'pimcore_workflow_button',
+                                menu: {
+                                    xtype: 'menu',
+                                    items: items
+                                }
                             });
+                            pimcore.layout.refresh();
 
                         }
-
-                        asset.toolbar.add({
-                            text: t('image_tagging_auto_classification'),
-                            scale: "medium",
-                            iconCls: 'image_tagging_white',
-                            cls: 'pimcore_workflow_button',
-                            menu: {
-                                xtype: 'menu',
-                                items: items
-                            }
-                        });
-                        pimcore.layout.refresh();
 
                     } else {
                         Ext.MessageBox.alert(data.message, data.reason);
